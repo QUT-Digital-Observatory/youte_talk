4fda5bde9e3ca65e9928e1dc7ae9ca2bda2bb5fc
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2023-05-31 16:19:48 +1000

    isort import libraries and blacken

--- youte_talk/transcriber.py
+++ youte_talk/transcriber.py
@@ -2,27 +2,38 @@
 Copyright: Digital Observatory 2023 <digitalobservatory@qut.edu.au>
 Author: Mat Bettinson <mat.bettinson@qut.edu.au>
 """
-from time import time
-from youte_talk.scraper import get_VideoDetails
-from os import remove, rename, path
-from youte_talk.whisper import WhisperTranscribe
-import youtube_dl
 from datetime import timedelta
+from os import path, remove, rename
+from time import time
+
+import youtube_dl
+
+from youte_talk.scraper import get_VideoDetails
+from youte_talk.whisper import WhisperTranscribe
+
 
 class Transcriber:
-    def __init__(self, url: str, output: str | None = None, model: str = 'small', fformat: str = 'small', saveaudio: bool = False, cpu: bool = False):
+    def __init__(
+        self,
+        url: str,
+        output: str | None = None,
+        model: str = "small",
+        fformat: str = "small",
+        saveaudio: bool = False,
+        cpu: bool = False,
+    ):
         """Set up a Transcription job"""
-        print('url:', url)
+        print("url:", url)
         self.url = url
-        self.output = url.split('=')[-1] if output is None else output 
+        self.output = url.split("=")[-1] if output is None else output
         self.model = model
         self.format = fformat
         self.saveaudio = saveaudio
-        self.fileext = 'txt' if fformat == 'text' else fformat
+        self.fileext = "txt" if fformat == "text" else fformat
         self.cpu = cpu
 
     def get_elapsed(self, start: float, end: float) -> str:
-        return str(timedelta(seconds=round(end-start)))
+        return str(timedelta(seconds=round(end - start)))
 
     def transcribe(self):
         """Execute the transcription job, saving output as appropriate"""
@@ -32,12 +43,11 @@ class Transcriber:
             print("Failed to get video details")
             return
         print("Got metadata in", self.get_elapsed(starttime, time()))
-        prompt = videodetails.title + ': ' + videodetails.shortDescription # Will feed to Whisper API
-        ydl_opts = {
-            'format': 'worstaudio[ext=webm]',
-            'outtmpl': '%(id)s.%(ext)s'
-        }
-        sourcefilename = videodetails.videoId + '.webm'
+        prompt = (
+            videodetails.title + ": " + videodetails.shortDescription
+        )  # Will feed to Whisper API
+        ydl_opts = {"format": "worstaudio[ext=webm]", "outtmpl": "%(id)s.%(ext)s"}
+        sourcefilename = videodetails.videoId + ".webm"
         if not path.isfile(sourcefilename):
             starttime = time()
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -48,25 +58,24 @@ class Transcriber:
         print("Transcribing...")
         transcription = WhisperTranscribe(sourcefilename, prompt, self.model, self.cpu)
         print("Whisper transcribed in", self.get_elapsed(starttime, time()))
-        outputfilename = self.output + '.' + self.fileext
+        outputfilename = self.output + "." + self.fileext
         outputdata = None
         match self.format:
-            case 'text':
+            case "text":
                 outputdata = transcription.text
-            case 'srt':
+            case "srt":
                 outputdata = transcription.srt
-            case 'json':
+            case "json":
                 outputdata = transcription.json
-            case 'csv':
+            case "csv":
                 outputdata = transcription.csv
         assert outputdata is not None
-        with open(outputfilename, 'w') as f:
+        with open(outputfilename, "w") as f:
             f.write(outputdata)
         # clean up
         if self.saveaudio:
-            outfile = self.output + '.webm'
+            outfile = self.output + ".webm"
             if sourcefilename != outfile:
                 rename(sourcefilename, outfile)
         else:
             remove(sourcefilename)
-     
