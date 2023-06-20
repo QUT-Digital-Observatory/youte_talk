041ffebd93a2b68f5786c4ed1e961991d621007e
Mat Bettinson <mat.bettinson@gmail.com>
2023-06-20 10:46:44 +1000

    linter formatting

--- youte_talk/cli.py
+++ youte_talk/cli.py
@@ -7,39 +7,85 @@ from sys import exit
 import subprocess
 import os
 import warnings
+
 warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
 warnings.filterwarnings("ignore", message=".*FP16 is not supported on CPU.*")
-warnings.filterwarnings("ignore", message=".*Performing inference on CPU when CUDA is available.*")
+warnings.filterwarnings(
+    "ignore", message=".*Performing inference on CPU when CUDA is available.*"
+)
+
 
 def transcribe(*args):
     from youte_talk.transcriber import Transcriber
+
     trans = Transcriber(*args)
     trans.transcribe()
     print("Done!")
+
 
 def is_ffmpeg_available():
     try:
         # The "which" command is used to find out if a system command exists.
         # It's used here to check if ffmpeg is available.
         # This works on Unix/Linux/Mac systems. On Windows, it's "where".
-        command = 'where' if os.name == 'nt' else 'which'
-        subprocess.check_output([command, 'ffmpeg'])
+        command = "where" if os.name == "nt" else "which"
+        subprocess.check_output([command, "ffmpeg"])
         return True
     except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
-whispermodels = ['tiny', 'tiny.en', 'base', 'base.en', 'small', 'small.en', 'medium', 'medium.en', 'large']
+
+whispermodels = [
+    "tiny",
+    "tiny.en",
+    "base",
+    "base.en",
+    "small",
+    "small.en",
+    "medium",
+    "medium.en",
+    "large",
+]
+
 
 @click.command()
-@click.argument('url')
-@click.option('--output', default=None, help="File name without an extension (extension determined by format), defaults to videoId")
-@click.option("--model", type=click.Choice(whispermodels, case_sensitive=True), default="small", help="Whisper model to use (defaults to small)")
-@click.option('--format', type=click.Choice(['text', 'srt', 'csv', 'json'], case_sensitive=False), default="text", help="Format of output file: text, srt or json")
-@click.option("--saveaudio", is_flag=True, show_default=True, default=False, help="Save the audio stream to a webm file")
-@click.option("--cpu", is_flag=True, show_default=True, default=False, help="Use the CPU instead of the GPU")
+@click.argument("url")
+@click.option(
+    "--output",
+    default=None,
+    help="File name without an extension (extension determined by format), defaults to videoId",
+)
+@click.option(
+    "--model",
+    type=click.Choice(whispermodels, case_sensitive=True),
+    default="small",
+    help="Whisper model to use (defaults to small)",
+)
+@click.option(
+    "--format",
+    type=click.Choice(["text", "srt", "csv", "json"], case_sensitive=False),
+    default="text",
+    help="Format of output file: text, srt or json",
+)
+@click.option(
+    "--saveaudio",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Save the audio stream to a webm file",
+)
+@click.option(
+    "--cpu",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Use the CPU instead of the GPU",
+)
 def main(url: str, output: str, model: str, format: str, saveaudio: bool, cpu: bool):
     """YouTalk: A command-line YouTube video transcriber using Whisper"""
-    if not url.startswith('https://youtube.com/watch?v=') and not url.startswith('https://www.youtube.com/watch?v='):
+    if not url.startswith("https://youtube.com/watch?v=") and not url.startswith(
+        "https://www.youtube.com/watch?v="
+    ):
         print("Youtube video URLs look like https://youtube.com/watch?v=...")
         exit(1)
 
@@ -51,6 +97,7 @@ def main(url: str, output: str, model: str, format: str, saveaudio: bool, cpu: b
         exit(1)
 
     transcribe(url, output, model, format, saveaudio, cpu)
+
 
 if __name__ == "__main__":
     main()
--- youte_talk/whisper.py
+++ youte_talk/whisper.py
@@ -11,6 +11,7 @@ import io
 from datetime import timedelta
 from torch.cuda import is_available
 
+
 # How hard was that OpenAI? Types. It's not hard.
 class WhisperSegment(TypedDict):
     id: int
@@ -24,6 +25,7 @@ class WhisperSegment(TypedDict):
     compression_ratio: float
     no_speech_prob: float
 
+
 @dataclass
 class Segment:
     id: int
@@ -31,100 +33,112 @@ class Segment:
     end: int
     text: str
 
+
 class WhisperTranscribe:
-    def __init__(self, sourcefile: str, prompt: str | None = None, modelname: str = "small", cpu: bool = False):
+    def __init__(
+        self,
+        sourcefile: str,
+        prompt: str | None = None,
+        modelname: str = "small",
+        cpu: bool = False,
+    ):
         self.segmentlist = []
         if cpu == False and is_available() == False:
             print("CUDA is not available. Falling back to CPU.")
             cpu = True
-        model = whisper.load_model(modelname, device='cpu' if cpu else 'cuda')
+        model = whisper.load_model(modelname, device="cpu" if cpu else "cuda")
         # This doesn't return what OpenAI says it does
         result = model.transcribe(sourcefile, initial_prompt=prompt)
         # It returns a dict, and we will store the verbose segments structure
-        self.segmentlist: list[WhisperSegment] = result['segments'] # type: ignore
+        self.segmentlist: list[WhisperSegment] = result["segments"]  # type: ignore
 
     @property
     def text(self) -> str:
-        return ''.join([segment['text'] for segment in self.segmentlist])
-    
+        return " ".join([segment["text"].strip() for segment in self.segmentlist])
+
     @property
     def json(self) -> str:
         objlist = []
         for segment in self.segmentlist:
-            objlist.append({
-                'id': segment['id'],
-                'start': round(segment['start'], 2),
-                'end': round(segment['end'], 2),
-                'text': segment['text'].strip()
-            })
+            objlist.append(
+                {
+                    "id": segment["id"],
+                    "start": round(segment["start"], 2),
+                    "end": round(segment["end"], 2),
+                    "text": segment["text"].strip(),
+                }
+            )
         return json.dumps(objlist, indent=2)
-    
+
     @property
     def segments(self) -> list[Segment]:
         short_segments = []
         for segment in self.segmentlist:
-            short_segments.append(Segment(
-                id=segment['id'],
-                start=round(segment['start'], 2),
-                end=round(segment['end'], 2),
-                text=segment['text'].strip()
-            ))
+            short_segments.append(
+                Segment(
+                    id=segment["id"],
+                    start=round(segment["start"], 2),
+                    end=round(segment["end"], 2),
+                    text=segment["text"].strip(),
+                )
+            )
         return short_segments
-    
+
     @property
     def csv(self) -> str | None:
         try:
             output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=['id', 'start', 'end', 'text'], quoting=csv.QUOTE_ALL)
+            writer = csv.DictWriter(
+                output, fieldnames=["id", "start", "end", "text"], quoting=csv.QUOTE_ALL
+            )
             writer.writeheader()
             for segment in self.segmentlist:
                 seg = {
-                    'id': segment['id'],
-                    'start': round(segment['start'], 2),
-                    'end': round(segment['end'], 2),
-                    'text': segment['text'].strip()
+                    "id": segment["id"],
+                    "start": round(segment["start"], 2),
+                    "end": round(segment["end"], 2),
+                    "text": segment["text"].strip(),
                 }
                 # encode special characters in the 'text' field
-                seg['text'] = seg['text'].encode('unicode_escape').decode('utf-8')
+                seg["text"] = seg["text"].encode("unicode_escape").decode("utf-8")
                 writer.writerow(seg)
             return output.getvalue()
         except Exception as e:
             print("An error occurred while creating the CSV string.")
             print(str(e))
             return None
-        
+
     @property
     def srt(self) -> str:
         srt_str = ""
 
         for i, segment in enumerate(self.segmentlist):
             # Convert start and end times to hh:mm:ss,sss format
-            start_time = timedelta(seconds=segment['start'])
-            end_time = timedelta(seconds=segment['end'])
+            start_time = timedelta(seconds=segment["start"])
+            end_time = timedelta(seconds=segment["end"])
 
             # Convert timedelta to appropriate string format
             str_start_time = str(start_time).zfill(8)
-            if '.' in str_start_time:
-                str_start_time = str_start_time.replace('.', ',')
-                if len(str_start_time.split(',')[1]) < 3:
-                    str_start_time = str_start_time + '0'*(3-len(str_start_time.split(',')[1]))
+            if "." in str_start_time:
+                str_start_time = str_start_time.replace(".", ",")
+                if len(str_start_time.split(",")[1]) < 3:
+                    str_start_time = str_start_time + "0" * (
+                        3 - len(str_start_time.split(",")[1])
+                    )
             else:
-                str_start_time = str_start_time + ',000'
+                str_start_time = str_start_time + ",000"
 
             str_end_time = str(end_time).zfill(8)
-            if '.' in str_end_time:
-                str_end_time = str_end_time.replace('.', ',')
-                if len(str_end_time.split(',')[1]) < 3:
-                    str_end_time = str_end_time + '0'*(3-len(str_end_time.split(',')[1]))
+            if "." in str_end_time:
+                str_end_time = str_end_time.replace(".", ",")
+                if len(str_end_time.split(",")[1]) < 3:
+                    str_end_time = str_end_time + "0" * (
+                        3 - len(str_end_time.split(",")[1])
+                    )
             else:
-                str_end_time = str_end_time + ',000'
+                str_end_time = str_end_time + ",000"
 
             # Add index, time range, and text to the string
             srt_str += f"{i+1}\n{str_start_time} --> {str_end_time}\n{segment['text'].strip()}\n\n"
 
         return srt_str
-
-
-
-
-    
